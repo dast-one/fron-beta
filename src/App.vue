296566783f9cc13b353c07017111779aa09e370e
Vital A Pi <code@vp.ag>
2022-05-15 10:26:25 +0300

    playground for dast gwork

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,14 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Dear <script>HA_X();'!<" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Scan request " Home</RouterLink>
+        <RouterLink to="/about">D1 " About</RouterLink>
       </nav>
     </div>
   </header>
