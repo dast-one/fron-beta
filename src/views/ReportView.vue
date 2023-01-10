1dd809e76bd1a793986757bf92853679b2b10104
Vital A Pi <code@vp.ag>
2023-01-10 14:11:04 +0300

    * override code color; * badges for alerts

--- src/views/ReportView.vue
+++ src/views/ReportView.vue
@@ -2,6 +2,8 @@
 
 <script setup>
 import Alert from '@/components/Alert.vue'
+
+import { CBadge } from '@coreui/vue'
 </script>
 
 <template>
@@ -14,16 +16,28 @@ import Alert from '@/components/Alert.vue'
     <div v-for="alert of alerts">
 
       <br><hr>
-      <h3 v-html="alert.name" />
+
+      <!-- <h3 v-html="alert.name" /> -->
+      <h5>
+        <CBadge v-if="alert.riskcode <= 1" color="info">FYI</CBadge>
+        <CBadge v-if="alert.riskcode == 2" color="warning">warn</CBadge>
+        <CBadge v-if="alert.riskcode == 3" color="danger">danger</CBadge>
+        {{ alert.name }}
+      </h5>
       <!-- <RouterLink :to="{ name: 'alert', params: { alert_id: alert.id }}">
         {{ alert.generated_ts }} </RouterLink> -->
+
       <p v-html="alert.description" />
+      <!-- <pre>{{ alert.description }}</pre> -->
+      <!-- <p> {{ alert.description }} </p> -->
 
       <br>
       <ul> <li v-for="ai of alert.instances" :key="ai.id">
       <code style="font-size: 80%"> @ {{ ai.method }} {{ ai.uri }} </code>
       <!-- <code v-if="ai.attack" style="font-size: 80%"> {{ai.attack}} </code> -->
       <!-- <code v-if="ai.evidence" style="font-size: 80%"> {{ai.evidence}} </code> -->
+      <pre style="font-size: 80%"><code>{{ ai.request_header }}{{ ai.request_body }}</code></pre>
+      <pre style="font-size: 80%"><samp>{{ ai.response_header }}{{ ai.response_body }}</samp></pre>
       </li> </ul>
 
       <br>
